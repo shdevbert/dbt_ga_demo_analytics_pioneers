58fbbf68db1ee2a194756443ea347149f706599d
Helena Steurer <helena.steurer@bergzeit.de>
2022-07-22 15:55:11 +0000

    WEBA-819 created analyses files

---
+++ analyses/02_event_params_coalesce.sql
@@ -0,0 +1,33 @@
+
+with ga4_event_params_unnested as (
+
+    select
+        parse_date('%Y%m%d', event_date) as date,
+        event_timestamp,
+        user_pseudo_id,
+        event_name,
+        device.category as page_device,
+        device.web_info.hostname as page_hostname,
+        event_params.key as key,
+        
+        /* recast all event values to string and select the first non-null value, to combine all in one column */
+        coalesce(
+            value.string_value,
+            cast(value.int_value as string),
+            cast(value.float_value as string),
+            cast(value.double_value as string)
+        ) as event_value_all_string
+        
+    from
+        {{ source('ga4_bz_overall', 'ga4_events_all') }},
+        unnest(event_params) AS event_params
+        
+    where
+    event_name in('select_item','view_item_list')
+
+    and _table_suffix not like '%intraday%'
+    and PARSE_DATE('%Y%m%d', _table_suffix) between {{ get_last_n_days_date_range(2) }}
+
+)
+
+select * from ga4_event_params_unnested
