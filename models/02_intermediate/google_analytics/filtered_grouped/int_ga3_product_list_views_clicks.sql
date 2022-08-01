014bccee9c043f1bd55285a6c84412a551c71c7d
Stephanie Hubert <stephanie.hubert@bergzeit.de>
2022-08-01 09:04:37 +0000

    WEBA-819: cleanup

--- models/02_intermediate/google_analytics/filtered_grouped/int_ga3_product_list_views_clicks.sql
+++ models/02_intermediate/google_analytics/filtered_grouped/int_ga3_product_list_views_clicks.sql
@@ -1,22 +1,22 @@
 with product_list_performance as(
-SELECT
-    date,
-    hostname,
-    page,
-    device,
-    productListName,
-    productListPosition,
-    session_id,
-    case when eventAction like ('eec.productImpression')
-        then eventAction 
-        else null 
-    end as list_impression,
-    case when eventAction like ('eec.productClick')
-        then eventAction 
-        else null 
-    end as list_click
+    select
+        date,
+        hostname,
+        page,
+        device,
+        productListName,
+        productListPosition,
+        session_id,
+        case when eventAction like ('eec.productImpression')
+            then eventAction 
+            else null 
+        end as list_impression,
+        case when eventAction like ('eec.productClick')
+            then eventAction 
+            else null 
+        end as list_click
 
-FROM {{ref('stg_ga3_products')}}
+    from {{ref('stg_ga3_products')}}
 )
 
 select * from product_list_performance
